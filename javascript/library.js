3ae8e5dea53960afb1da7ec9259dab4da8abdcb5
Kenneth Posey <kenneth.posey@gmail.com>
2014-11-24 16:32:01 -0800

    Add inline js and style

--- javascript/library.js
+++ javascript/library.js
@@ -1,14 +1,20 @@
 
-//Didn't feel like a refactor so this exists though I will be using the new (better) name
-var RandomIdGenerator = function(length) { return RandomTextGenerator(length); };
+// Didn't feel like a refactor so this exists though
+// I will be using the new (better) name
+var RandomIdGenerator = function(length)
+{ return RandomTextGenerator(length); };
 
 var RandomTextGenerator = function(length) {
     var text = "";
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                     + "abcdefghijklmnopqrstuvwxyz"
+                     + "0123456789";
 
     for ( var i=0; i < length; i++ ) {
-        text += characters.charAt(Math.floor(Math.random() * characters.length));
+        text += characters.charAt(
+                    Math.floor(Math.random() * characters.length)
+                  );
     }
-    
+
     return text;
 };
